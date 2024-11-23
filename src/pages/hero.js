e15da227463679a298e0e5d58fb0559a164f09db
Donna <skies09@hotmail.com>
2024-11-23 16:46:51 +0000

    update transitions

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -38,12 +38,18 @@ const Hero = () => {
 
 	const renderButton = () => {
 		return (
-			<Button
-				text="Contact"
-				className="font-fredoka px-6 py-2 bg-colorOne text-colorFour border-2 border-colorFour rounded-xl tracking-wider font-semibold lg:overflow-hidden shadow-[0_10px_20px_rgba(0,0,0,0.25),0_6px_6px_rgba(0,0,0,0.22)]"
-				active={true}
-				action={"contact"}
-			/>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1, delay: 0.5 }}
+			>
+				<Button
+					text="Contact"
+					className="font-fredoka px-6 py-2 bg-colorOne text-colorFour border-2 border-colorFour rounded-xl tracking-wider font-semibold lg:overflow-hidden shadow-[0_10px_20px_rgba(0,0,0,0.25),0_6px_6px_rgba(0,0,0,0.22)]"
+					active={true}
+					action={"contact"}
+				/>
+			</motion.div>
 		);
 	};
 
@@ -55,19 +61,24 @@ const Hero = () => {
 			>
 				{/* Image Section */}
 
-				<div className="w-full md:w-1/2 h-[40vh] md:h-full flex items-start md:items-center justify-center">
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 0.5 }}
+					className="w-full md:w-1/2 h-[40vh] md:h-full flex items-start md:items-center justify-center"
+				>
 					<img
 						src="../../assets/images/walking.jpg"
 						alt="Hero Background"
 						className="flex w-full md:max-w-[80%] max-h-[80%] object-cover min-h-[15rem] md:object-contain border-b-4 border-colorTwo md:border-2 md:border-colorTwo md:rounded-xl md:shadow-[0_10px_20px_rgba(0,0,0,0.25),0_6px_6px_rgba(0,0,0,0.22)]"
 					/>
-				</div>
+				</motion.div>
 
 				{/* Text and Button Section */}
 				<div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start px-6 md:pl-12 lg:pl-16">
 					<motion.div
 						className="text-center md:text-left"
-						initial={{ opacity: 0, x: -200 }}
+						initial={{ opacity: 0, x: 0 }}
 						animate={{ opacity: 1, x: 0 }}
 						transition={{ duration: 1 }}
 					>
